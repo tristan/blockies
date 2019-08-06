7994ed679804af31320f16ece1004a352ed9cd83
Tristan King <tristan.king@gmail.com>
2019-08-06 14:07:42 +0200

    Update Pillow to 6.1.0

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='blockies',
-    version='0.0.1',
+    version='0.0.2',
     author='Tristan King',
     author_email='tristan.king@gmail.com',
     py_modules=['blockies'],
     url='http://github.com/tristan/blockies',
     description='A tiny library for generating blocky identicons.',
+    long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
     install_requires=open('requirements.txt').read()
 )
